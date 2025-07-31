51745a6d6ac890b01eace4d33c30ff062d0463f2
Lavanyarekkala <77536811+Lavanyarekkala@users.noreply.github.com>
2025-07-31 00:09:55 -0400

    Binary-Search-1

--- Solution2.java
+++ Solution2.java
@@ -1,15 +1,15 @@
-/** Time Complexity: log m +log k (m to identify the space within which the target is present and then log k for binary search in the identified space)
+/** Time Complexity: O(log m + log k) (m to identify the space within which the target is present and then log k for binary search in the identified space)
 * Space Complexity: O(1) [No extra space is used, only variables are used]
 * Did this code successfully run on Leetcode : Yes
 * Any problem you faced while coding this : No
 * LeetCode Problem: Search in infinite Sorted Array
 * Approach:
-* 1. Use exponential search to find the range where the target might be present.
+* 1. Double the search space until the target is within the bounds.
 * 2. Once the range is identified, perform binary search within that range.
 * Problem Link: https://leetcode.com/problems/search-in-a-sorted-array-of-unknown-size/
 */
 
-class SearchInfiniteArray {
+class Solution2 {
     public int search(ArrayReader reader, int target) {
         int low = 0, high = 1;
 
